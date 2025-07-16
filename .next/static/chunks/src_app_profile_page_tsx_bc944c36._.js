31a4e6abc815bbd17a86baa21fb4c0af198d820d
Gulshan Kumar Nayak <gulnayak1206@gmail.com>
2025-07-16 20:08:12 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_bc944c36._.js 1752676692471

---
+++ .next/static/chunks/src_app_profile_page_tsx_bc944c36._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_8312f372._.js"
+],
+    source: "dynamic"
+});
